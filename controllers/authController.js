feb62ed150848d36531483150e89965d16f8282b
GameYojoies <game@yojoies.com>
2024-07-25 14:46:53 +0700

    #

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt'); // นำเข้า expressJwt
-
+require('dotenv').config();
 // โหลดค่าจากไฟล์ .env
 const dotenv = require('dotenv');
 dotenv.config();
@@ -11,6 +11,7 @@ exports.protect = expressJwt({
   userProperty: "auth"
 });
 
+console.log(process.env.SECRET_KEY,"secret");
 exports.login = (req, res) => {
   const { username, password } = req.body;
   if (password === process.env.PASSWORD) {
