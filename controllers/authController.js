0ba10141965f46f40d65fc38c6535adcd3ce2064
GameYojoies <game@yojoies.com>
2024-07-25 14:22:15 +0700

    ๒

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,24 +2,25 @@ const jwt = require('jsonwebtoken');
 const expressJwt = require('express-jwt'); // นำเข้า expressJwt
 
 // โหลดค่าจากไฟล์ .env
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 exports.protect = expressJwt({
-  secret: process.env.SECRET_KEY, // ใช้ SECRET_KEY จาก environment variables
-  algorithms: ["HS256"], // อัลกอริธึมที่ใช้ในการเข้ารหัส
-  userProperty: "auth" // ชื่อ property ที่จะเก็บข้อมูลผู้ใช้
+  secret: process.env.SECRET_KEY,
+  algorithms: ["HS256"],
+  userProperty: "auth"
 });
 
 exports.login = (req, res) => {
-  const { username, password } = req.body; // ดึงข้อมูล username และ password จาก request body
-  if (password === process.env.PASSWORD.trim()) { // ตรวจสอบรหัสผ่าน
-    const token = jwt.sign({ username }, process.env.SECRET_KEY.trim(), {
-      expiresIn: "1d", // ตั้งค่าการหมดอายุของ token เป็น 1 วัน
+  const { username, password } = req.body;
+  if (password === process.env.PASSWORD) {
+    const token = jwt.sign({ username }, process.env.SECRET_KEY, {
+      expiresIn: "1d",
     });
-    return res.json({ token, username }); // ส่ง token และ username กลับไป
+    return res.json({ token, username });
   } else {
     return res.status(400).json({
-      error: "Invalid password", // หากรหัสผ่านไม่ถูกต้อง
+      error: "Invalid password",
     });
   }
 };
